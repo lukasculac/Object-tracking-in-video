e98d4b4097bb62f8307b4f4bf0f765877938670a
lukasculac <luka.sculac@gmail.com>
2023-03-02 22:14:45 +0100

    3 parts. One using webcam to object track, one using a video to object track and one counting cars in one section of video

--- Webcam-traking.py
+++ Webcam-traking.py
@@ -1,11 +1,14 @@
+import numpy
 from ultralytics import YOLO
 import cv2
 import cvzone
 import math
 
-cap = cv2.VideoCapture("Images/test_video.mp4")  # For Video
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
 
-model = YOLO("yolo-weights/yolov8n.pt")
+model = YOLO('yolo-weights/yolov8n.pt')
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
@@ -19,26 +22,27 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush"
               ]
 
-
 while True:
     success, img = cap.read()
+    results = model(img, stream = True)
 
-    results = model(img, stream=True)
     for r in results:
         boxes = r.boxes
         for box in boxes:
             # Bounding Box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
             w, h = x2 - x1, y2 - y1
             cvzone.cornerRect(img, (x1, y1, w, h))
+
             # Confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
             # Class Name
             cls = int(box.cls[0])
 
+            #join confidence and calss to box
             cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
+
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
